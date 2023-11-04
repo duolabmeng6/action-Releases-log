aba3ce413df68acddc977d09213ed2537d5c537f
多啦b梦 <1715109585@qq.com>
2023-11-05 04:17:00 +0800

    1

--- help.py
+++ help.py
@@ -10,6 +10,13 @@ def Github输出变量多行文本(name, value):
     GITHUB_OUTPUT = os.getenv("GITHUB_OUTPUT", "")
     with open(GITHUB_OUTPUT, "a") as file:
         file.write(f"{name}<<EOF\n{value}\nEOF\n")
+    
+    # 读取这个文件内容看看
+    with open(GITHUB_OUTPUT, 'r') as file:
+        content = file.read()
+        print(content)
+
+
 
 def Github输出状态(name,value):
     GITHUB_STATE = os.getenv("GITHUB_STATE", "")
